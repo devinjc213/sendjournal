a5391b56f34174d969f77006004ec716e94f2b07
devinjc213 <devinjc213@gmail.com>
2019-05-11 16:12:31 -0500

    commit attempt

--- src/components/Step3.js
+++ src/components/Step3.js
@@ -10,6 +10,33 @@ class Step3 extends React.Component {
     return (
       <div>
         <label>What type of climbing will you be doing?</label>
+        <input
+          type="checkbox"
+          name="isBoulderChecked"
+          checked={this.props.isBoulderChecked}
+          onChange={this.props.handleChange}
+        /> Bouldering
+        <br />
+        <input
+          type="checkbox"
+          name="isTopChecked"
+          checked={this.props.isTopChecked}
+          onChange={this.props.handleChange}
+        /> Top Rope
+        <br />
+        <input
+          type="checkbox"
+          name="isLeadChecked"
+          checked={this.props.isLeadChecked}
+          onChange={this.props.handleChange}
+        /> Lead
+        <br />
+        <input
+          type="checkbox"
+          name="isTradChecked"
+          checked={this.props.isTradChecked}
+          onChange={this.props.handleChange}
+        /> Trad
         <br />
       </div>
     )
